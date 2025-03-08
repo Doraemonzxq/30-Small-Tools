7b1baf570218ac37dbc0f85c365565af0542c95c
Doraemon <1799182045@qq.com>
2025-03-08 17:47:26 +0800

    First_Rename

---
+++ ReName/ReName/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Paket;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReName
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string ? _folderPath;
+        [ObservableProperty]
+        private string ? _renameRule;
+        [ObservableProperty]
+        private ObservableCollection<FileMode> _files;
+        public MainWindowViewModel()
+        {
+
+        }
+        [RelayCommand]
+        public void SelectFolderCommand(string[] paths)
+        {
+            Files.Clear();
+            foreach (var path in paths)
+            {
+                if (File.Exists(path))
+                {
+                    Files.Add(new FileItem(path));
+                }
+            }
+        }
+        [RelayCommand]
+        public void ApplyRenameCommand() { }
+    }
+}
---
+++ ReName/ReName/Models/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReName.Models
+{
+    public class FileModel:ObservableObject
+    {
+        private string  OldFilePath { get; set; }
+        private string  NewFilePath { get; set; }
+    }
+}
